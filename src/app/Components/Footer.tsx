3a444872394d8ed3da95e3e5bbf5f6d4b20a61f0
Tamiris73 <btamiris73@gmail.com>
2023-11-07 18:55:48 -0300

    primeiro commit pagina inicial

---
+++ src/app/Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+      <div className="w-full">
+        <img src="./img/Frame.png" alt="" />
+      </div>
+  );
+};
+
+export default Footer;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        verde: '#13DFDC',
+        azul:' #08A1E4',
+        preto: '#0A065C', 
+        cinza:'#ECECEC'
+    },
+    fontFamily: {
+      'lobster': ['Lobster', 'cursive'],
+    },
     },
   },
   plugins: [],
